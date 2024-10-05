63b01f84fc3a19ffb1e160e94dc6af16d038b1f9
Леус Вадим Олександрович <ipz223_lvo@student.ztu.edu.ua>
2024-10-05 11:11:25 +0300

    Add Navbar component and display screen width

---
+++ ReactJS-LAB-02/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to the Responsive Navbar Example</h1>
+        <p>Resize the screen to see the navigation change!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
