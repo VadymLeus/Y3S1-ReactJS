fba8ee5a9e5fa1b700480acbaf4a985a1d62bf3f
Леус Вадим Олександрович <ipz223_lvo@student.ztu.edu.ua>
2024-10-06 21:23:26 +0300

    додано схему валідацї форми з використанням yup

--- ReactJS-LAB-03/src/flightValidation.js
+++ ReactJS-LAB-03/src/flightValidation.js
@@ -3,9 +3,13 @@ import * as yup from 'yup';
 const flightValidation = yup.object().shape({
   from: yup.string().required("Місце відправлення є обов'язковим"),
   to: yup.string().required("Місце прибуття є обов'язковим"),
-  departureDate: yup.date().required("Дата вильоту є обов'язковою"),
+  departureDate: yup
+    .date()
+    .typeError('Дата вильоту має бути дійсною датою')
+    .required("Дата вильоту є обов'язковою"),
   returnDate: yup
     .date()
+    .typeError('Дата повернення має бути дійсною датою')
     .min(yup.ref('departureDate'), "Дата повернення має бути після дати вильоту")
     .required("Дата повернення є обов'язковою"),
 });
